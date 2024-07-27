bd47126c800e7ee798299302f1d763aeeba88411
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-07-27 13:43:57 +0430

    refactor: Improve code structure and add orbit controls for camera

--- hello-three-js/main.js
+++ hello-three-js/main.js
@@ -1,27 +1,45 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
+// Initialize the scene
 const scene = new THREE.Scene();
 
-const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-const cubeMaterial = new THREE.MeshBasicMaterial({ color: "blue" });
+// Initialize the cube
+const cubeGeometery = new THREE.BoxGeometry(1, 1, 1);
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(cubeGeometery, cubeMaterial);
 
-const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+scene.add(cube);
 
-scene.add(cubeMesh);
-
-// Camera
+// Initialize the camera
 const camera = new THREE.PerspectiveCamera(
-  35,
+  45,
   window.innerWidth / window.innerHeight,
   0.1,
   35,
 );
 camera.position.z = 5;
-scene.add(camera);
 
-// Renderer
+// Initialize the renderer
 const canvas = document.querySelector(".three");
-const renderer = new THREE.WebGLRenderer({ canvas: canvas });
-
+const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.render(scene, camera);
+
+// Initalize the orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.autoRotate = true;
+controls.autoRotateSpeed = 15;
+
+// Animate the scene
+function animate() {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+
+  camera.updateProjectionMatrix();
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+animate();
